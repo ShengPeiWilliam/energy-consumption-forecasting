57ac4e639e73ef54dc0a14d391b9754f2abc387e
William <willc23@uci.edu>
2026-06-30 19:10:43 -0700

    feat: add data exploration notebook and config

---
+++ code/config.py
@@ -0,0 +1,7 @@
+ZONES = ['Zone 1 Power Consumption', 'Zone 2 Power Consumption', 'Zone 3 Power Consumption']
+ZONE_LABELS = ['Zone 1', 'Zone 2', 'Zone 3']
+WEATHER = ['Temperature', 'Humidity', 'Wind Speed', 'general diffuse flows', 'diffuse flows']
+PALETTE = ['#2166ac', '#d6604d', '#4dac26']
+
+SPLIT_DATE = '2017-11-01'
+DATA_PATH = '../data/Tetuan_City_Power_Consumption.csv'
