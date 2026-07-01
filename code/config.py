0754ddb770cee06b2158b1f4c3a554f3ad66af5f
William <willc23@uci.edu>
2026-06-30 21:19:16 -0700

    feat: add HORIZONS constant and update config

--- code/config.py
+++ code/config.py
@@ -2,6 +2,13 @@ ZONES = ['Zone 1 Power Consumption', 'Zone 2 Power Consumption', 'Zone 3 Power C
 ZONE_LABELS = ['Zone 1', 'Zone 2', 'Zone 3']
 WEATHER = ['Temperature', 'Humidity', 'Wind Speed', 'general diffuse flows', 'diffuse flows']
 PALETTE = ['#2166ac', '#d6604d', '#4dac26']
+HORIZONS = {
+    '10 min':  1,
+    '1 hour':  6,
+    '6 hours': 36,
+    '1 day':   144,
+    '1 week':  1008,
+}
 
 SPLIT_DATE = '2017-11-01'
 DATA_PATH = '../data/Tetuan_City_Power_Consumption.csv'
